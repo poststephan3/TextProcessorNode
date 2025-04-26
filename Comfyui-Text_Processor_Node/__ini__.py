a31fc14a809ba8e041382b325b74d73319893026
poststephan3 <157274162+poststephan3@users.noreply.github.com>
2025-04-26 08:47:22 +0200

    Update and rename __ini__.py to __init__.py

--- Comfyui-Text_Processor_Node/__ini__.py
+++
@@ -1,8 +0,0 @@
-# Node-Registrierung
-NODE_CLASS_MAPPINGS = {
-    "TextProcessorNode": TextProcessorNode
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "TextProcessorNode": "🔄 Advanced Text Processor"
-}
---
+++ Comfyui-Text_Processor_Node/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+
+from .TextProcessorNode import TextProcessorNode
+
+NODE_CLASS_MAPPINGS = {
+    "TextProcessorNode": TextProcessorNode
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+  "TextProcessorNode": "🔄 Advanced Text Processor"}
